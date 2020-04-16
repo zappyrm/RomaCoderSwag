b645d545b76a3bf2af042b0fcca12b3a7ef38962
Romeu  Bonini Neto <romeuboninineto@iMac-de-Romeu.local>
2020-04-15 23:30:43 -0300

    preparei a activity_main para a listView e criei o category_list_item

--- app/src/main/java/com/example/romacoderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/romacoderswag/Controller/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.romacoderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.romacoderswag.Model.Category
 import com.example.romacoderswag.R
+import com.example.romacoderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+               android.R.layout.simple_list_item_1,
+               DataService.categories)
+
+        categoryListView.adapter = adapter
+
     }
 }
--- app/src/main/java/com/example/romacoderswag/Model/Category.kt
+++ app/src/main/java/com/example/romacoderswag/Model/Category.kt
@@ -1,3 +1,9 @@
 package com.example.romacoderswag.Model
 
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String){
+
+    override fun toString(): String {
+        return title
+    }
+
+}
