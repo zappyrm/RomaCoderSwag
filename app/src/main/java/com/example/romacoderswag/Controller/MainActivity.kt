7b60fae2c4025870597299352066be99438e32c7
Romeu  Bonini Neto <romeuboninineto@iMac-de-Romeu.local>
2020-04-20 05:22:15 -0300

    mudei as views da main_activity

--- app/src/main/java/com/example/romacoderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/romacoderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.romacoderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.romacoderswag.Adapters.CategoryAdapter
 import com.example.romacoderswag.Model.Category
 import com.example.romacoderswag.R
 import com.example.romacoderswag.Services.DataService
@@ -10,16 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-               android.R.layout.simple_list_item_1,
-               DataService.categories)
-
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
 
     }
--- app/src/main/java/com/example/romacoderswag/Services/DataService.kt
+++ app/src/main/java/com/example/romacoderswag/Services/DataService.kt
@@ -10,6 +10,14 @@ object DataService {
         Category("SHIRTS", "shirtimage"),
         Category("HOODIES", "hoodieimage"),
         Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
         Category("DIGITAL", "digitalgoodsimage")
 
     )
