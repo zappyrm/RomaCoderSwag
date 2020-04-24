5581c4b4a3a81a107c4f35b4326e7048ee79439d
Romeu  Bonini Neto <romeuboninineto@iMac-de-Romeu.local>
2020-04-24 11:32:40 -0300

    add Product_list_item.xml e add click nas categorias

--- app/src/main/java/com/example/romacoderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/romacoderswag/Controller/ProductsActivity.kt
@@ -1,18 +1,43 @@
 package com.example.romacoderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.romacoderswag.Adapters.ProductsAdapter
 import com.example.romacoderswag.R
+import com.example.romacoderswag.Services.DataService
 import com.example.romacoderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE){
+              spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720){
+            spanCount = 3
+        }
+
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
 
     }
 }
--- app/src/main/java/com/example/romacoderswag/Services/DataService.kt
+++ app/src/main/java/com/example/romacoderswag/Services/DataService.kt
@@ -27,10 +27,19 @@ object DataService {
         Product("Devslopes Graphic Beanie", "$18", "hat1"),
         Product("Devslopes Hat Black", "$20", "hat2"),
         Product("Devslopes Hat White", "$18", "hat3"),
+        Product("Devslopes Hat Snapback", "$22", "hat4"),
+        Product("Devslopes Graphic Beanie", "$18", "hat1"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat White", "$18", "hat3"),
         Product("Devslopes Hat Snapback", "$22", "hat4")
+
 
     )
     val hoodies = listOf(
+        Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+        Product("Devslopes Hoodie Red", "$32", "hoodie2"),
+        Product("Devslopes Gray Hoodie", "$28", "hoodie3"),
+        Product("Devslopes Black Hoodie", "$32", "hoodie4"),
         Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
         Product("Devslopes Hoodie Red", "$32", "hoodie2"),
         Product("Devslopes Gray Hoodie", "$28", "hoodie3"),
@@ -41,10 +50,25 @@ object DataService {
         Product("Devslopes Badge Light Gray", "$20", "shirt2"),
         Product("Devslopes Logo Shirt Red", "$28", "shirt3"),
         Product("Devslopes Hustle", "$22", "shirt4"),
+        Product("Kickflip Studios", "18", "shirt5"),
+        Product("Devslopes Shirt Black", "$18", "shirt1"),
+        Product("Devslopes Badge Light Gray", "$20", "shirt2"),
+        Product("Devslopes Logo Shirt Red", "$28", "shirt3"),
+        Product("Devslopes Hustle", "$22", "shirt4"),
         Product("Kickflip Studios", "18", "shirt5")
     )
 
-    val digitalGood = listOf<Product>()
+    val digitalGood = listOf<Product>(
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat Black", "$20", "hat2")
+
+    )
 
     fun getProducts(category: String) : List<Product>{
 
